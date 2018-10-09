6615c28265d4c996b6726a71176eb8b1243e445a
Dmitrii <dmitrykharkovsky@gmail.com>
2018-10-10 00:50:55 +0800

    refact 8_2

--- ch_8/8_2.py
+++ ch_8/8_2.py
@@ -1,34 +1,31 @@
 def find_path_main(mines, r, c):
     if r == 0 or c == 0:
         raise Exception("wrong dimensions")
-    memo = {(0, 0): True}
-    ans = find_path(mines, (r - 1, c - 1), memo)
-    print(memo)
-    return ans
+    failed_points = []
+    path = []
+    ans = find_path(mines, (r - 1, c - 1), failed_points, path)
+    if ans:
+        return path
+    return None
 
 
-def find_path(mines, curr, memo):
-        if curr in mines:
-            return False, []
-        if curr in memo.keys():
-            return memo[curr], []
-
+def find_path(mines, curr, failed_points, path):
         c1, c2 = curr
+        if curr in mines or curr in failed_points or c1 < 0 or c2<0:
+            return False
 
-        if c1 < 0 or c2 < 0:
-                return False, []
-        a1, p1 = find_path(mines, (c1 - 1, c2), memo)
-        a2, p2 = find_path(mines, (c1, c2 - 1), memo)
-        a = a1 or a2
-        memo[curr] = a
-        if not a:
-                return False, []
-        p = p1 if a1 else p2
-        return True, p + [curr]
+        if curr == (0,0) or \
+                find_path(mines, (c1 - 1, c2), failed_points, path) or\
+                find_path(mines, (c1, c2-1), failed_points, path):
+            path.append(curr)
+            return True
+
+        failed_points.append(curr)
+        return False
 
 
 if __name__ == "__main__":
     r = 2
     c = 3
-    mines = [(0,1),]
+    mines = [(0,1), (0,2)]
     print(find_path_main(mines,r,c))
